a7ebf656495a25ad1a30217fa078eacfddee216d
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-16 05:16:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,6 +5,14 @@ class Anagram
     @word = word
   end
     
+  def match(find_anagram_in_this_string) 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
+      end
+    end 
+  end
   
     
     
