b549e3e93b8ffcce55e92042a9dd0d81750cdd96
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-16 05:15:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,12 @@ class Anagram
   end
     
   def match(array_of_words)
-    
+    return_array = []
+    array_of_words.each do |element|
+      (@word.split("").sort) == (element.split("").sort)
+        return_array << element
+    end
+  end
     
     
     
